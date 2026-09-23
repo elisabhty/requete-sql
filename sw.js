1289e56d69f2297100726a08afb9a7cb92ce8dd3
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 00:29:03 +0200

    Refonte UI/UX du cours Clé étrangère

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-pkcomp-gap-v1027';
+const CACHE = 'requete-2026-09-24-fk-course-ux-v1028';
 
 const PRECACHE = [
   './',
