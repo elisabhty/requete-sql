c605d79a8bc56926466cea0032e16a44f0b55691
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 00:57:10 +0200

    Refonte complète du cours INNER JOIN

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-joins-steps-v1029';
+const CACHE = 'requete-2026-09-24-inner-lab-v1030';
 
 const PRECACHE = [
   './',
