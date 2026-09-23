d05e1c3ef571aeea9d69a3302a8c5a936151dccc
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 00:50:21 +0200

    Refonte du chapitre Jointures en étapes numérotées

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-fk-course-ux-v1028';
+const CACHE = 'requete-2026-09-24-joins-steps-v1029';
 
 const PRECACHE = [
   './',
