e3fdc0d118b3d6db0e6729639c51d7da8dcf9dcf
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 01:12:08 +0200

    INNER JOIN : retire l'erreur fréquente

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-anno-v1033';
+const CACHE = 'requete-2026-09-24-inner-nopiege-v1034';
 
 const PRECACHE = [
   './',
