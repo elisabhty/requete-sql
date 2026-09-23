d1f3e7d5c8aa29549da581a3b53ba1924e345e69
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 01:10:32 +0200

    INNER JOIN : requête annotée ligne par ligne

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-need-v1032';
+const CACHE = 'requete-2026-09-24-inner-anno-v1033';
 
 const PRECACHE = [
   './',
