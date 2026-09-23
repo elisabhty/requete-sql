a374d224d2c846e9e36649b38854964e70ee7476
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 01:25:50 +0200

    INNER JOIN : retire le bloc réflexe

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-distinct-v1037';
+const CACHE = 'requete-2026-09-24-inner-noreflex-v1038';
 
 const PRECACHE = [
   './',
