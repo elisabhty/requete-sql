d48be1ee23d48ec043cbea5c4cde41398461e3ff
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 00:12:04 +0200

    Change la version du cache pour diffuser l'avertissement cle etrangere

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-22-inner-cards-v1021';
+const CACHE = 'requete-2026-09-24-fk-warning-v1022';
 
 const PRECACHE = [
   './',
