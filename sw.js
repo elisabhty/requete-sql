973c9ca23e558e63c3732f97d5a195067ce4e50e
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 09:01:53 +0200

    INNER JOIN : rôles de ON puis WHERE

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-nathan-v1039';
+const CACHE = 'requete-2026-09-24-inner-onwhere-v1040';
 
 const PRECACHE = [
   './',
