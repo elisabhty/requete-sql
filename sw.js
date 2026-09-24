acbbb305cf88de58de4bd5a37ef3e441b71d677b
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 09:07:28 +0200

    Jointures : texte sans trous en fin de ligne

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-14-v1041';
+const CACHE = 'requete-2026-09-24-join-wrap-v1042';
 
 const PRECACHE = [
   './',
