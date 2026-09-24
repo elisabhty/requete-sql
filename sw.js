81d8422317ef39a12d844965e527fe4d831e64d9
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 22:54:11 +0200

    LEFT JOIN : schéma entonnoir Mission accomplie

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-left-cap-v1064';
+const CACHE = 'requete-2026-09-24-left-funnel-v1065';
 
 const PRECACHE = [
   './',
