7db2d8373a63a2f1049d1fe0ee653d2e9c35304e
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 09:27:32 +0200

    INNER JOIN : nouveau texte du cours

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-more-v1046';
+const CACHE = 'requete-2026-09-24-inner-v3-v1047';
 
 const PRECACHE = [
   './',
