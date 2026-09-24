d32b64aa8c83e922771fcdb8d27e3080ae20e047
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 21:07:43 +0200

    INNER JOIN : fusionne les étapes redondantes

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-hasard-v1054';
+const CACHE = 'requete-2026-09-24-inner-dedup-v1055';
 
 const PRECACHE = [
   './',
