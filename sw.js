7aaf4ed843177ddf20d3e3010b3849461f9880f7
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 09:04:54 +0200

    INNER JOIN : 14 commandes en juillet

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-onwhere-v1040';
+const CACHE = 'requete-2026-09-24-inner-14-v1041';
 
 const PRECACHE = [
   './',
