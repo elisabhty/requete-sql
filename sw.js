712286ad4123d2d1fcc8d89439c7f2f9fb7b9d8e
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 21:27:58 +0200

    INNER JOIN : carte Mission accomplie

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-notes-v1059';
+const CACHE = 'requete-2026-09-24-inner-done-v1060';
 
 const PRECACHE = [
   './',
