54fc1de89cf35e3a5cc6f59666ed8cde1730e3bc
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-24 09:44:49 +0200

    INNER JOIN : passe UI/UX (animation interactive, étapes, accessibilité)

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-24-inner-run-v1048';
+const CACHE = 'requete-2026-09-24-inner-uxpass-v1049';
 
 const PRECACHE = [
   './',
