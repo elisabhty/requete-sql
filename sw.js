99f7639bad3d794f1b0d115b52a34f83078bfd2a
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-25 21:51:57 +0200

    LEFT JOIN : simplifie la comparaison pour Inès

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-25-phrase-v1089';
+const CACHE = 'requete-2026-09-25-left-ines-v1090';
 
 const PRECACHE = [
   './',
