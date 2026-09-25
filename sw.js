f7f5b0c8ce38d86a7a14ca750df9c84b011b027f
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-25 17:48:11 +0200

    Défilement depuis la barre du bas + plus de trous en fin de ligne sur iPhone

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-25-on-multiple-v1081';
+const CACHE = 'requete-2026-09-25-navscroll-v1082';
 
 const PRECACHE = [
   './',
