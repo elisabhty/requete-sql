e654dcd68a60be36d83d5635625134148e2d5f36
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-25 23:59:11 +0200

    JOIN + GROUP BY : phrase d'intro du regroupement clarifiée

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-25-count-v1094';
+const CACHE = 'requete-2026-09-25-groupe-v1095';
 
 const PRECACHE = [
   './',
