5926631d4c03d7cb0227881d1ddb603c8115790d
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-26 13:24:15 +0200

    Carte +1 : animation plus rapide

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-26-etapes2-v1101';
+const CACHE = 'requete-2026-09-26-plus1fast-v1102';
 
 const PRECACHE = [
   './',
