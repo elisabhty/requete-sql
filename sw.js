efe83bf053c2bb3e9cac9ffc622dd9706b029087
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-26 23:50:45 +0200

    Cours : moins d'espace en bas des étapes

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-27-alias2-v1121';
+const CACHE = 'requete-2026-09-27-gap-v1122';
 
 const PRECACHE = [
   './',
