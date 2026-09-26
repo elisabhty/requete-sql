cb7f30946a79c9bd51e6121599f4d97a1fcabb61
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-26 13:47:08 +0200

    Carte +1 : déclenchement au centre

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-26-tables-v1106';
+const CACHE = 'requete-2026-09-26-plus1view-v1107';
 
 const PRECACHE = [
   './',
