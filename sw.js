2cad7545bc62cfd59c01eab80aa83a9a838859de
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-27 00:02:30 +0200

    Cadres À retenir plus clairs

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-27-nopers2-v1125';
+const CACHE = 'requete-2026-09-27-memo-v1126';
 
 const PRECACHE = [
   './',
