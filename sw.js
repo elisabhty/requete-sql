69c11c5040776d8872fe3d36514280cf9fbb1868
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-26 23:46:14 +0200

    SELF JOIN : alias reformulés

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-27-mission2-v1119';
+const CACHE = 'requete-2026-09-27-alias-v1120';
 
 const PRECACHE = [
   './',
