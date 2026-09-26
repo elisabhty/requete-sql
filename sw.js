cdc40751359d7ee0529696e7a1e8f8cc715b8010
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-26 23:48:12 +0200

    SELF JOIN : note alias

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-27-alias-v1120';
+const CACHE = 'requete-2026-09-27-alias2-v1121';
 
 const PRECACHE = [
   './',
