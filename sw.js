df7f9c5925763c438460127f33d415cb71a86e76
elisabhty <47713994+elisabhty@users.noreply.github.com>
2026-09-26 23:53:31 +0200

    Objectif : emoji 📌

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
   CACHE change à chaque déploiement des ressources. APP_VERSION concerne
   les données utilisateur : ne pas le modifier pour un rafraîchissement. */
 
-const CACHE = 'requete-2026-09-27-gap-v1122';
+const CACHE = 'requete-2026-09-27-objectif-v1123';
 
 const PRECACHE = [
   './',
